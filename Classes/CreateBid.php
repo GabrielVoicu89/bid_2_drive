722b27cf4bcb5a667086c5f32bd522e30fae6378
GabrielVoicu89 <g_the_reaper@yahoo.com>
2023-06-19 18:51:18 +0200

    bid almost finish

--- Classes/CreateBid.php
+++ Classes/CreateBid.php
@@ -7,7 +7,7 @@ include_once __DIR__ . "/../DB/Database.php";
 
 
 use Db\Database;
-
+use PDO;
 
 
 class CreateBid
@@ -23,8 +23,68 @@ class CreateBid
 
     public function create_bid($car_id, $bid, $bidder_id)
     {
+        // creating conection with database
         $dbh = Database::createDBConnection();
-        $query = $dbh->prepare("INSERT INTO `bids_history`(`car_id`,`bid`,`bidder_id`) VALUES (?,?,?)");
-        $query->execute([$car_id, $bid, $bidder_id]);
+        $query = $dbh->prepare("SELECT * FROM `cars` WHERE `id` = ? ");
+        $query->execute([$car_id]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result[0]['auction_end'] < date('Y-m-d H:i:s')) {
+            echo "You can't bid anymore since the auction ended.";
+        } else {
+            // select * from bids_history where car_id = GET['id']
+            $query = $dbh->prepare("SELECT * FROM `bids_history` WHERE `car_id` = ? ");
+            $query->execute([$car_id]);
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            // if count(array) = 0 means no bids
+            if (count($result) === 0) {
+                // if no bids for  car id
+                // select from cars
+                $query = $dbh->prepare("SELECT * FROM `cars` WHERE `id` = ? ");
+                $query->execute([$car_id]);
+                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                // highest bid = price + bid
+                $final_price = $result[0]['price'] + $bid;
+                $query = $dbh->prepare("INSERT INTO `bids_history`(`car_id`,`bid`,`bidder_id`,`final_price`) VALUES (?,?,?,?)");
+                $query->execute([$car_id, $bid, $bidder_id, $final_price]);
+            } else {
+
+                // $final_price = $bid + "last final price";
+                $last_bid = end($result);
+                $final_price = $last_bid['final_price'] + $bid;
+                $query = $dbh->prepare("INSERT INTO `bids_history`(`car_id`,`bid`,`bidder_id`,`final_price`) VALUES (?,?,?,?)");
+                $query->execute([$car_id, $bid, $bidder_id, $final_price]);
+            }
+            // show bids
+            $query = $dbh->prepare("SELECT * FROM `bids_history` WHERE `car_id` = ? ");
+            $query->execute([$car_id]);
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            var_dump(end($result));
+        }
+    }
+
+    public static function check_status($car_id)
+    {
+        $dbh = Database::createDBConnection();
+        $query = $dbh->prepare("SELECT * FROM `cars` WHERE `id` = ? ");
+        $query->execute([$car_id]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result[0]['auction_end'] < date('Y-m-d H:i:s')) {
+            echo "The auction ended.";
+        }
+    }
+
+    public static function show_bids($car_id)
+    {
+        $dbh = Database::createDBConnection();
+        $query = $dbh->prepare("SELECT * FROM `bids_history` WHERE `car_id` = ? ");
+        $query->execute([$car_id]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        if (count($result) === 0) {
+            echo "This auction has no bids yet.";
+        } else {
+            var_dump(end($result));
+        }
     }
 }
--- create_bid_index.php
+++ create_bid_index.php
@@ -16,17 +16,27 @@ use Classes\CreateBid;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>create_bid</title>
-
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <?php
+    require_once __DIR__ . "/Menu/nav.php"
+    ?>
+
     <form action="create_bid_index.php?id=<?php echo $_GET['id'] ?>" method="POST">
         <input type="text" id="bid" class="form-control" name="bid" required />
         <input type="submit" value="envoyer">
     </form>
     <?php
 
-    var_dump($_GET['id']);
+
 
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -39,9 +49,13 @@ use Classes\CreateBid;
             $_POST["bid"],
             $_SESSION['userid']
         );
+    } else {
+        CreateBid::show_bids($_GET['id']);
+        echo '<br>';
+        CreateBid::check_status($_GET['id']);
     }
     ?>
-
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
 </body>
 
 </html>
